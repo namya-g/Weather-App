e8bed5c2a9fe97b9fa2920a611e83f4016f195f8
namya-g <namyagupta15@gmail.com>
2026-07-09 22:12:30 +0530

    git add . git commit -m git push

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("JS file is connected ")
+const apiKey="b87d8cfc4b9d42608b8183759260807";
+const input=document.getElementById("city_name");
+const search=document.getElementById("search");
+search.addEventListener("click",() =. {
+    let city=input.value;
+});
