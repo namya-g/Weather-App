4454625359a84aa6855511e847f67b1df3e0f006
namya-g <namyagupta15@gmail.com>
2026-07-10 01:45:31 +0530

    git add . git commit -m git push

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ console.log("JS file is connected ")
 const apiKey="b87d8cfc4b9d42608b8183759260807";
 const input=document.getElementById("city_name");
 const search=document.getElementById("search");
+const hourly=document.getElementById("hourly");
+const hcard=document.getElementById("hour-card");
+const ten_day=document.getElementById("hourly");
+const hcard=document.getElementById("hour-card");
 search.addEventListener("click",() => {
     let city=input.value;
     console.log(city);
@@ -23,6 +27,20 @@ async function weather(city) {
     auv.innerText= data.location.uv;
     const awind= document.getElementById("wind");
     awind.innerText= data.location.wind_kph;
-    const awind= document.getElementById("wind");
-    awind.innerText= data.location.wind_kph;
-}
+    const arain= document.getElementById("${chance_of_rain}%");
+    arain.innerText= data.location.wind_kph;
+    const avsbt= document.getElementById("vsbt");
+    avsbt.innerText= data.location.vis_km;
+    const ahmdt= document.getElementById("hmdt");
+    ahmdt.innerText= data.location.humidity;
+    const arise= document.getElementById("rise");
+    arise.innerText= data.forecast.forecastday[0].astro.sunrise;
+    const aset= document.getElementById("set");
+    aset.innerText= data.forecast.forecastday[0].astro.sunset;
+    data.forecast.forecastday[0].hour.forEach((hdata) => {
+        const clone= hcard.contentEditable.cloneNode(true);
+        clone.querySelector(".htemp").innerText= `${hour.temp_c}°C`
+        clone.querySelector(".hrain").innerText= `${hour.chance_of_rain}%`
+        clone.querySelector(".hour").innerText= hour.time.split(" ")[1];
+    }
+)};
